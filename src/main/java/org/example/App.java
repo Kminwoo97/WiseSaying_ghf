83c62eb9982268568a0dbbf71ae2c8102d26b8c6
Kminwoo97 <kmo971130@gmail.com>
2023-03-06 15:37:30 +0900

    명언 등록시 마다 번호증가

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -14,18 +14,24 @@ public class App {
     public void run() {
         System.out.println("== 명언 앱 ==");
 
+        long lastWiseSayingId = 0L;
         while(true){
             System.out.print("명령) ");
             String cmd = sc.nextLine().trim();
 
             if(cmd.isEmpty())
                 continue;
-
-
             switch (cmd){
                 case "종료":
                     System.out.println("프로그램이 종료되었습니다.");
                     return;
+                case "등록":
+                    System.out.println("명언 : ");
+                    String content = sc.nextLine().trim();
+                    System.out.println("작가 : ");
+                    String authorName = sc.nextLine().trim();
+
+                    System.out.println(++lastWiseSayingId + "번 명언이 등록되었습니다.");
                 default:
                     System.out.println("올바르지 않은 명령입니다." + cmd);
                     break;
