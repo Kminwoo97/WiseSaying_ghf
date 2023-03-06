df0a2d856f7bd191e41fe486ab048e8c95697fad
Kminwoo97 <kmo971130@gmail.com>
2023-03-06 14:22:34 +0900

    테스트케이스, 출력을 모니터에 하지 않고 문자열로 얻기

--- src/test/java/org/example/TestUtil.java
+++ src/test/java/org/example/TestUtil.java
@@ -1,13 +1,26 @@
 package org.example;
 
-import java.io.ByteArrayInputStream;
+import java.io.*;
 import java.util.Scanner;
 
 public class TestUtil {
     public static Scanner genScanner(String input){
-        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        return new Scanner(input);
+    }
 
-        return new Scanner(in);
+    public static ByteArrayOutputStream setOutToByteArray() {
+        final ByteArrayOutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
 
+        return output;
+    }
+
+    public static void clearSetOutToByteArray(ByteArrayOutputStream output) {
+        System.setOut(new PrintStream(new FileOutputStream(FileDescriptor.out)));
+        try {
+            output.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
